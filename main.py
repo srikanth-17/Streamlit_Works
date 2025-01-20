8cc8cf9ff96c03aaf17726ca5c0f27cd2a0fbd4c
srikanth <srikanthrajalingam555@gmail.com>
2025-01-20 14:08:12 +0530

    Files Worked

---
+++ main.py
@@ -0,0 +1,16 @@
+# import streamlit as st
+# import numpy as np
+# st.sidebar.title("Stream1it ")
+# st.sidebar.header(":blue[lntroduction to Databases]")
+# st.sidebar.subheader(" Web Applications")
+# data = np.random.randn(10, 1)
+# with st.expander("Chart",expanded=True):
+#     st.subheader("Expander with chart")
+#     st.line_chart(data)
+# with st.expander("Data",expanded=False):
+#     st.subheader("Expander with data")
+#     st.write(data)
+
+"""
+            Hello guys This is main.py file where I use this file to run and test 
+"""
